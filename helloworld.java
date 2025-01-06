9719fdc2765080d91249d42beb755bbf851e84a6
Yuvrajpatil0411 <104100362+Yuvrajpatil0411@users.noreply.github.com>
2025-01-06 21:44:05 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch37 team with PRADIP, Chetan , SACHIN , NIKHIL");
+     System.out.println("Hello batch37 team with velocity batch 1");
       System.out.println("Hello Batch batch38 WITH FOR STUDENTS RUNNING in front of me and sumit in making CI automated vikas teach devops CI proecss");
     }  
 } 
